00c67403da941cf0b71b36de54bb21780aa7440b
ozll2b <kyr4515@naver.com>
2022-03-31 23:53:18 +0900

    3/29

---
+++ class3/BOJ1992.py
@@ -0,0 +1,39 @@
+n=int(input())
+graph=[]
+for _ in range(n):
+    graph.append(list(map(int,input())))
+
+answer=''
+def quadtree(n,r):
+    global answer
+    x,y=r
+    o,z=0,0
+
+    if n==1:
+        if graph[x][y]==1:
+            answer +='1'
+        else:
+            answer +='0'
+        return
+
+    for i in range(x,x+n):
+        for j in range(y,y+n):
+            if graph[i][j]==1:
+                o +=1
+            else:
+                z+=1
+            if o*z>0:
+                break
+    if o==n**2:
+        answer +='1'
+    elif z==n**2:
+        answer +='0'
+    else:
+        answer +='('
+        for i in range(x,x+(n//2)+1,n//2):
+            for j in range(y, y+(n // 2) + 1, n // 2):
+                quadtree(n//2,(i,j))
+        answer +=')'
+    return
+quadtree(n,(0,0))
+print(answer)
